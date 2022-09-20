73e7e681adf20ef40e309b1e68622be25e9e96bd
cleverplant <cleverplant39@gmail.com>
2022-09-20 11:43:44 +0200

    добавилось - пока сделаю паузу - по GB тяжелые задачи

---
+++ Детерминированность рандом примеры/Program.cs
@@ -0,0 +1,18 @@
+﻿Random rndu = new Random();
+var output = rndu.Next(0, 10);
+var outputAsNumber = Convert.ToInt32(output);
+Console.WriteLine(outputAsNumber);
+
+
+Random rnd = new Random();//Создание объекта для генерации чисел
+int value = rnd.Next();//Получить случайное число 
+Console.WriteLine(value);
+
+//Получить случайное число (в диапазоне от 0 до 10)
+int value1 = rnd.Next(0, 10); 
+Console.WriteLine(value1);
+
+//Создание объекта для генерации чисел (с указанием начального значения)
+Random rndum = new Random(24);
+int value3 = rndum.Next();
+Console.WriteLine(value3);
---
+++ Пример создания метода/Program.cs
@@ -0,0 +1,38 @@
+﻿// === Создадим наш первый метод. ===
+
+//Его задача - вывести на экран текущую дату:
+//Today is: 2021-10-25
+
+// Объявление класса
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class App
+{
+    // Определение метода
+    public static void ShowCurrentDate()
+    {
+        // Класс со свойством для получения текущего времени
+        var currentDate = DateTime.Now;
+        var text = $"Today is: {currentDate}";
+        Console.WriteLine(text);
+    }
+}
+
+
+ 
+// namespace GetTime
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             //Текущие дата/время хранятся в свойстве Now класса DateTime
+ 
+//             Console.WriteLine(DateTime.Now + " holla"); //Получаем текущие дату и время, выводим их в консоль
+//             //Чтобы программа сразу же не закрылась
+//             Console.ReadKey();
+//         }
+//     }
+// }
